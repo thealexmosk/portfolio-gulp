e8e4c448d58b675fbdb899f6e35de902751d476c
alexMosk <thealexmosk@gmail.com>
2019-01-13 13:32:56 +0200

    pathes fix

--- public/assets/js/main.js
+++
@@ -1,7 +0,0 @@
-new fullpage('#fullpage', {
-	licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
-  anchors: ['main', 'about', 'skills', 'edu_exp', 'contact'],
-	autoScrolling:true,
-	scrollOverflow: true,
-	scrollBar: false
-});
